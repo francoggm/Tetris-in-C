09ee350266372f8f6ca69013ee831525648c3225
Gabriel Franco <82771295+francoggm@users.noreply.github.com>
2021-11-19 19:52:59 -0300

    Add files via upload

--- componentes/menuSelecao.c
+++ componentes/menuSelecao.c
@@ -6,22 +6,22 @@ int menuSelecao()
     Vector2 mouse = {x, y};
 
     mouse = GetMousePosition();
-    if ((mouse.x > 580 && mouse.x < 735) && (mouse.y > 350 && mouse.y < 380) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    if ((mouse.x > 550 && mouse.x < 705) && (mouse.y > 350 && mouse.y < 380) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
     {
         return 1;
     }
 
-    else if ((mouse.x > 545 && mouse.x < 775) && (mouse.y > 400 && mouse.y < 430) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    else if ((mouse.x > 515 && mouse.x < 745) && (mouse.y > 400 && mouse.y < 430) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
     {
         return 2;
     }
 
-    else if ((mouse.x > 570 && mouse.x < 740) && (mouse.y > 450 && mouse.y < 480) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    else if ((mouse.x > 540 && mouse.x < 710) && (mouse.y > 450 && mouse.y < 480) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
     {
         return 3;
     }
 
-    else if ((mouse.x > 625 && mouse.x < 680) && (mouse.y > 500 && mouse.y < 530) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    else if ((mouse.x > 595 && mouse.x < 650) && (mouse.y > 500 && mouse.y < 530) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
     {
         return 4;
     }
